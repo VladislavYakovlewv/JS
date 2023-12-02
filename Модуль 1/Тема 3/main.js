8244f3878cadc0112a9eaec77716cc212005ccdb
Vladislav <jakovlewv@yandex.ru>
2023-12-02 13:59:03 +0300

    Добавлен einstein.js

--- Модуль 1/Тема 3/main.js
+++ Модуль 1/Тема 3/main.js
@@ -4,9 +4,8 @@
     const Category = "electronics";
     const Cost = 60000;
     
-    console.log(ProductName);
-    
-    console.log(Cost * Quantity);
+    console.log('Наименование товара: ', ProductName);
+    console.log('Общая стоймость товаров:', Cost * Quantity);
 }
 
 {
@@ -14,4 +13,20 @@
     const Quantity = 7;
     const Category = "electronics";
     const Cost = 8000;
+}
+
+{
+
+    const ProductName = prompt('Введите наименование товара: ', '');
+    const Quantity = +prompt('Введите количество товара: ', '');
+    const Category = prompt('Введите категорию товара: ', '');
+    const Cost = +prompt('Введите цену товара: ', '');
+
+    console.log('Наименование: ', typeof ProductName);
+    console.log('Категория: ', typeof Category);
+    console.log('Количество товара: ', typeof Quantity);
+    console.log('Цена: ', typeof Cost);
+    console.log(`На складе ${Quantity} единицы товара "${ProductName}" на сумму ${Cost * Quantity} рублей`);
+
+    
 }
