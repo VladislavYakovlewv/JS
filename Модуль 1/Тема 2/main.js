ddefdc68b78e8d91d8a2e666d818aa82e34633a8
Vladislav <jakovlewv@yandex.ru>
2023-12-02 13:27:50 +0300

    Inital

---
+++ Модуль 1/Тема 2/main.js
@@ -0,0 +1,17 @@
+{
+    let ProductName = "PC";
+    let Quantity = 12;
+    let Category = "electronics";
+    let Cost = 60000;
+    
+    console.log(ProductName);
+    
+    console.log(Cost * Quantity);
+}
+
+{
+    let ProductName = "Camera";
+    let Quantity = 7;
+    let Category = "electronics";
+    let Cost = 8000;
+}
---
+++ Модуль 1/Тема 3/main.js
@@ -0,0 +1,17 @@
+{
+    const ProductName = "PC";
+    const Quantity = 12;
+    const Category = "electronics";
+    const Cost = 60000;
+    
+    console.log(ProductName);
+    
+    console.log(Cost * Quantity);
+}
+
+{
+    const ProductName = "Camera";
+    const Quantity = 7;
+    const Category = "electronics";
+    const Cost = 8000;
+}
